965fcc48c75a50c4b545c2052f3a418f9bafce38
Antonio Musciano <71357098+antoniomusciano@users.noreply.github.com>
2021-05-26 16:53:14 -0400

    saving events to local storage
    5-26-21

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let events = localStorage.getItem('events') ? JSON.parse(localStorage.getItem('e
 const calendar = document.getElementById('calendar');
 const newEventModal = document.getElementById('newEventModal');
 const backDrop = document.getElementById('modalBackDrop');
+const eventTitleInput = document.getElementById('eventTitleInput');
 const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 
@@ -14,7 +15,7 @@ function openModal(date){
     const eventForDay = events.find(e => e.date === clicked);
 
     if (eventForDay) {
-        console.log('Event Exists')
+        newEventModal.style.display = 'block';
 
     } else {
 
@@ -86,9 +87,48 @@ function load(){
     
 
 }
+function closeModal() {
+
+    eventTitleInput.classList.remove('error');
+    newEventModal.style.display = 'none';
+    backDrop.style.display = 'none';
+    eventTitleInput.value = '';
+    clicked = null;
+    load();
+
+}
 
 
-function initButtons(){
+function saveEvent() {
+
+    if (eventTitleInput.value) {
+
+        eventTitleInput.classList.remove('error');
+
+        events.push({
+
+            date: clicked,
+            title: eventTitleInput.value,
+
+        });
+
+        localStorage.setItem('events', JSON.stringify(events));
+        closeModal();
+
+    } else {
+
+        eventTitleInput.classList.add('error');
+    }
+
+}
+
+
+
+
+
+
+
+function initButtons() {
 
 
     document.getElementById('nextButton').addEventListener('click', () => {
@@ -102,6 +142,9 @@ function initButtons(){
         nav--;
         load();
     });
+
+    document.getElementById('saveButton').addEventListener('click',saveEvent);
+    document.getElementById('cancelButton').addEventListener('click',closeModal);
 
 
 
